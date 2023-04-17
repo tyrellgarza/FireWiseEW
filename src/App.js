6218fde32a33e766c46443fe66b78fecd0ed90f5
Tyrell Garza <tyrellgarza@t-MBP-14.local>
2023-04-16 18:04:23 -0700

    added nav bar, blank pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/structure/navbar';
+import HomePage from './components/pages/HomePage';
+import AboutUs from './components/pages/AboutUs';
+import Resources from './components/pages/Resources';
+import Impact from './components/pages/Impact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/homepage" element={<HomePage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/impact" element={<Impact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
