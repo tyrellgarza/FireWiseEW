c3e408ff8ae3db4ad05a0160f3db3133b2e8636c
ANOKHI_SHAH <ANOKHI15@UW.EDU>
2023-04-28 15:18:13 -0700

    added team member photos

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './aboutus.css';
 
 const AboutUs = () => {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div classname="about-us">
+      <div id= "left">
+        <h1>Why we are doing this?</h1>
+      </div>
+      <div id="right">
+      <h1>The team</h1>
+        <div id="allmembers">
+          <div className="member">
+            <h2>Anokhi Shah</h2>
+          </div>
+          <div className="member">
+            <h2>Tyrell Garza</h2>
+          </div>
+          <div className="member">
+            <h2>Fatima Laghari</h2>
+          </div>
+          <div className="member">
+            <h2>Dellina Gebrehiwot</h2>
+          </div>
+          <div className="member">
+            <h2>Kathy Thin</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
