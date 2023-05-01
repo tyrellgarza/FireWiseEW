cfcae411d1bc77977f4d16b253c783968880aff6
kthin <kathythin@yahoo.com>
2023-05-01 01:08:48 -0700

    impact edits

--- src/components/pages/Impact.js
+++ src/components/pages/Impact.js
@@ -4,10 +4,13 @@ import './impact.css';
 
 const Impact = () => {
   return (
-    <div>
+    <div className="content">
       <h1>Impact</h1>
-      <div>
+      <div className="section-timeline">
         <h1>Biggest Wildfires to Date in Eastern Washington</h1>
+      </div>
+      <div className="section-funding">
+        <h1>Fundraising and Donation Opportunities</h1>
       </div>
     </div>
   );
