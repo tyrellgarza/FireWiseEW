eace47a51e062457fc69a299b93c5af08fe090f0
ANOKHI_SHAH <ANOKHI15@UW.EDU>
2023-04-30 17:09:46 -0700

    about us images

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -11,19 +11,23 @@ const AboutUs = () => {
       <h1>The team</h1>
         <div id="allmembers">
           <div className="member">
-           <img src="/images/AnokhiShah.jpeg" alt="Team Member Anokhi Shah"></img>
+            <img src="/images/AnokhiShah.jpg" alt="Team Member Anokhi Shah"></img>
             <h2>Anokhi Shah</h2>
           </div>
           <div className="member">
+            <img src="/images/TyrellGarza.jpg" alt="Team Member Tyrell Garza"></img>
             <h2>Tyrell Garza</h2>
           </div>
           <div className="member">
+            <img src="/images/FatimaLaghari.jpg" alt="Team Member Fatima Laghari"></img>
             <h2>Fatima Laghari</h2>
           </div>
           <div className="member">
+            <img src="/images/DellinaGebrehiwot.jpg" alt="Team Member Dellina Gebrehiwot"></img>
             <h2>Dellina Gebrehiwot</h2>
           </div>
           <div className="member">
+            <img src="/images/KathyThin.jpg" alt="Team Member Kathy Thin"></img>
             <h2>Kathy Thin</h2>
           </div>
         </div>
