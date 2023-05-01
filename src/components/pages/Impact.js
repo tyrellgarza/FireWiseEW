7a867f05cbe201d35e7ba32a03f363afae0bdb8a
kthin <kathythin@yahoo.com>
2023-05-01 00:47:09 -0700

    impact edits

--- src/components/pages/Impact.js
+++ src/components/pages/Impact.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './impact.css';
+
 
 const Impact = () => {
   return (
     <div>
       <h1>Impact</h1>
+      <div>
+        <h1>Biggest Wildfires to Date in Eastern Washington</h1>
+      </div>
     </div>
   );
 };
