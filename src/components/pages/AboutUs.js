db2deac6770635d73d3963b9da488745313ba3fc
ANOKHI_SHAH <ANOKHI15@UW.EDU>
2023-05-09 17:16:11 -0500

    why we are doing this section

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import './aboutus.css';
+
 /** About us section */
 const AboutUs = () => {
   return (
     <div classname="about-us">
       <div id="left">
+        <div id="header">
         <h1>Why we are doing this?</h1>
+        </div>
+        <p> We believe that residents of Eastern Washington can benefit from
+            a resource like this in times of peril. Being a region particularly
+            prone to wildfires, which are often unpredictable and destructive.
+            In recent years, the incidence and intensity of wildfires in this region
+            have been on the rise.
+        </p>
       </div>
+
       <div id="right">
         <h1>The team</h1>
         <div id="allmembers">
           <div className="member">
-            <img src="/images/AnokhiShah.jpg" alt="Team Member Anokhi Shah"></img>
+            <img src="/images/ShahAnokhi.jpg" alt="Team Member Anokhi Shah"></img>
             <h2>Anokhi Shah</h2>
           </div>
           <div className="member">
-            <img src="/images/TyrellGarza.jpg" alt="Team Member Tyrell Garza"></img>
+            <img src="/images/GarzaTyrell.jpg" alt="Team Member Tyrell Garza"></img>
             <h2>Tyrell Garza</h2>
           </div>
           <div className="member">
