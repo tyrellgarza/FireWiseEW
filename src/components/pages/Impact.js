7820628931115ddf7f93eb942d79181f2382162a
ANOKHI_SHAH <ANOKHI15@UW.EDU>
2023-05-13 22:47:54 -0700

    structure for timeline and change to about us

--- src/components/pages/Impact.js
+++ src/components/pages/Impact.js
@@ -3,6 +3,18 @@ import './impact.css';
 
 
 const Impact = () => {
+  // Get all timeline events
+  const timelineEvents = document.querySelectorAll('.timeline-event');
+
+  // Add click event listener to each event
+  timelineEvents.forEach(event => {
+    event.addEventListener('click', () => {
+      // Toggle active class to show/hide event details
+      const description = event.querySelector('.info');
+      description.classList.toggle('hidden');
+    });
+  });
+
   return (
     <div className="content">
       <h1>Impact</h1>
@@ -12,6 +24,37 @@ const Impact = () => {
       </p>
       <div className="section-timeline">
         <h1>Biggest Wildfires to Date in Eastern Washington</h1>
+        <div id="timeline">
+          <div class="timeline-event">
+            <div class="event-date">2017</div>
+            <div class="event-description">Test 1</div>
+            <div className='info'>
+              <p>this is a test</p>
+            </div>
+          </div>
+          <div class="timeline-event">
+            <div class="event-date">2018</div>
+            <div class="event-description">info</div>
+          </div>
+          <div class="timeline-event">
+            <div class="event-date">2019</div>
+            <div class="event-description">info</div>
+          </div>
+          <div class="timeline-event">
+            <div class="event-date">2020</div>
+            <div class="event-description">info</div>
+          </div>
+          <div class="timeline-event">
+            <div class="event-date">2021</div>
+            <div class="event-description">info</div>
+          </div>
+          <div class="timeline-event">
+            <div class="event-date">2022</div>
+            <div class="event-description">info</div>
+          </div>
+
+        </div>
+
       </div>
       <div className="section-funding">
         <h1>Fundraising and Donation Opportunities</h1>
