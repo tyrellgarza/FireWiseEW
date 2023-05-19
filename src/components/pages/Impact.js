9c104d3eaf90fb7c326ceb1294ca9f5c133b2965
kthin <kathythin@yahoo.com>
2023-05-18 23:28:50 -0700

    impact edits + links

--- src/components/pages/Impact.js
+++ src/components/pages/Impact.js
@@ -43,6 +43,8 @@ const Impact = () => {
       <h3 className="caption">South of Lind, Washington. August 2022.</h3>
       <div className="section-timeline">
         <h1 className="title2">Biggest Wildfires to Date in Eastern Washington</h1>
+        <p> The below timeline illustrates the biggest wildfires to date in Eastern Washington from the years 2017 through 2022.
+        </p>
         <div id="timeline">
           {data && data.map((year, index) => <Year key={index} data={year} />)}
         </div>
@@ -52,12 +54,21 @@ const Impact = () => {
         <p> There are a number of fundraising and donation resources to help support the impact of Eastern Washington wildfires.
           Listed below are funding opportutnities to help rebuild and restore affected communities.
         </p>
+        <h2>Go Fund Me</h2>
+        <a href="https://www.gofundme.com/c/act/wildfire-relief/washington">
+          How to Help Those Affected by the Washington Fires
+        </a> <br></br>
+        <h2>Red Cross</h2>
+        <a href="https://www.redcross.org/local/washington/about-us/our-work/western-wildfires.html">
+          Western Wildfires: Northwest Region
+        </a> <br></br>
+        <h2>KXLY</h2>
+        <a href="https://www.kxly.com/news/local-news/here-s-how-you-can-donate-to-victims-of-washington-s-wildfires/article_13848719-be59-5214-bbdf-af33b08ac008.html">
+          Here's How You Can Donate to Victims
+        </a> <br></br>
       </div>
     </div>
   );
 };
 
 export default Impact;
-
-/* https://www.gofundme.com/c/act/wildfire-relief/washington
-https://www.redcross.org/local/washington/about-us/our-work/western-wildfires.html */
